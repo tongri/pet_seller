049bbae389f9e66083dedb74b7da122f26004a85
AntonHryb <anton.hryb.ddi@gmail.com>
2021-10-01 16:04:37 +0300

    cors

--- server/pet/settings.py
+++ server/pet/settings.py
@@ -138,5 +138,5 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3030',
+    'http://localhost:3000',
 ]
