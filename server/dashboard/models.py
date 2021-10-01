e87b52eb46e706d0390085b8c34d9c5819bbf7f7
werfit <s25112003@gmail.com>
2021-10-01 15:54:02 +0300

    useless commit

--- server/dashboard/models.py
+++ server/dashboard/models.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 
 
-class MyUser(User):
+class MyUser(AbstractUser):
     username = models.CharField(max_length=20)
